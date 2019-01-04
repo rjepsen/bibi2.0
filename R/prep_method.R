b49d64711d946af6da76b7c99986086578a0177b
Rikke Jepsen <RJepsen@icprb.int>
2019-01-04 06:50:22 -0500

    Markdown and prep method.

--- R/prep_method.R
+++ R/prep_method.R
@@ -7,7 +7,7 @@
 
 prep_method <- function(long.df) {
   final.df <- long.df %>%
-    dplyr::mutate(month = format(date, "%m")) %>%
+    dplyr::mutate(month = format(date, "%m"), month = as.integer(month)) %>%
     dplyr::filter(strahler_stream_order <= 4,
                   g_method %in% c(49, 57, 58, 59, 86,
                                 87, 89, 92, 93, 94, 101, 103, 104),
